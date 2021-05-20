0016442ba8edc6c65557440c8143a388e864a819
Kokat111 <kacperorczykowksi@gmail.com>
2021-05-20 16:36:55 +0200

    v2

--- app/forms.py
+++ app/forms.py
@@ -3,5 +3,5 @@ from wtforms import StringField,SubmitField
 from wtforms.validators import DataRequired,Length,Email,EqualTo,ValidationError
 
 class MiastoForm(FlaskForm):
-	miasto = StringField('misato',validators=[DataRequired(),Length(min=2,max=40)])
-	submit = SubmitField('Szukaj')
+	miasto = StringField('Misato',validators=[DataRequired(),Length(min=2,max=40)])
+	submit = SubmitField('submit')
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,27 @@
 from app.forms import MiastoForm
-from flask import render_template
+from flask import render_template,redirect,url_for
 from app import app
+import requests
+
+city=''
+
+def get_key():
+	url = 'http://dataservice.accuweather.com/locations/v1/cities/search?apikey=wPAADHiPqogW497qyheQpEGimLAhTHsj&q={}'
+	
+	r = requests.get(url.format(city)).json()
+	data = {
+		'key':r[0]['Key'],
+	}
+	return data
 
 @app.route('/')
 def home():
-   return "hello world!"
+	form = MiastoForm()
+	city = form.miasto.data
+	print(city)
+	if form.validate_on_submit():
+		r = get_weather_data([0])
+		print(r)
+
+	return render_template('search.html',title='Search',form=form)
+
