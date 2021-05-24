f717408398e8443403c1064487bd67816ebf67ad
Kokat111 <kacperorczykowksi@gmail.com>
2021-05-24 13:08:25 +0200

    V5

--- app/routes.py
+++ app/routes.py
@@ -1,28 +1,29 @@
 from app.forms import MiastoForm
-from flask import render_template,redirect,url_for
+from flask import render_template,redirect,url_for,request
 from app import app
 import requests
 
-responce=requests.get('http://dataservice.accuweather.com/locations/v1/cities/search?apikey=wPAADHiPqogW497qyheQpEGimLAhTHsj&q=krakow').json()
-data = {
-		responce[0]['Key'],
-	}
-print(data)
 
-def get_key():
-	url = 'http://dataservice.accuweather.com/locations/v1/cities/search?apikey=wPAADHiPqogW497qyheQpEGimLAhTHsj&q={}'
-	
-	r = requests.get(url.format(city)).json()
-	data = {
-		'key':r[0]['Key'],
-	}
-	return data
 
-@app.route('/', methods=["GET"])
+
+@app.route('/', methods=['GET','POST'])
 def home():
+
+
 	form = MiastoForm()
-
-
+	if form.validate_on_submit():
+		global city
+		city = request.form.get('miasto')
+		responce = requests.post('http://dataservice.accuweather.com/locations/v1/cities/search?apikey=wPAADHiPqogW497qyheQpEGimLAhTHsj&q='+city+'').json()
+		global data
+		data = responce[0]['Key'],
+	print(data)
+	print(city)
+	return redirect(url_for('forcast'))
 
 	return render_template('search.html',title='Search',form=form)
 
+@app.route('/pogoda', methods=['GET','POST'])
+def forcast():
+	
+	return print(data)
