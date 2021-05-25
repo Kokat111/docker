c43ac0009ee5e997f5c297aac58cfc3fcf6ef142
Kokat111 <kacperorczykowksi@gmail.com>
2021-05-25 12:05:24 +0200

    V6

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,9 @@ WORKDIR /app
 
 COPY . /app
 
-RUN pip3 --no-cache-dir install -r requirements.txt                                                                            
+RUN pip3 --no-cache-dir install -r requirements.txt     
+
+EXPOSE 5000                                                                       
 
 ENTRYPOINT  ["python3"]
 
--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,8 @@ from flask import render_template,redirect,url_for,request
 from app import app
 import requests
 
-
+city = ''
+data = 0
 
 
 @app.route('/', methods=['GET','POST'])
@@ -16,14 +17,29 @@ def home():
 		city = request.form.get('miasto')
 		responce = requests.post('http://dataservice.accuweather.com/locations/v1/cities/search?apikey=wPAADHiPqogW497qyheQpEGimLAhTHsj&q='+city+'').json()
 		global data
-		data = responce[0]['Key'],
-	print(data)
-	print(city)
-	return redirect(url_for('forcast'))
+		data = responce[0]['Key']
+		print(data)
+		print(city)
+		return redirect(url_for('forcast'))
 
 	return render_template('search.html',title='Search',form=form)
 
 @app.route('/pogoda', methods=['GET','POST'])
 def forcast():
+	form = MiastoForm()
+	forcast_info = requests.post('http://dataservice.accuweather.com/forecasts/v1/daily/5day/'+data+'?apikey=%20%09wPAADHiPqogW497qyheQpEGimLAhTHsj%20&metric=true').json()
+
+	day1 = forcast_info['DailyForecasts'][0]['Date'],str(forcast_info['DailyForecasts'][0])
+
 	
-	return print(data)
+	day2 = forcast_info['DailyForecasts'][1]['Date']
+
+	day3 = forcast_info['DailyForecasts'][2]['Date']
+
+	day4 = forcast_info['DailyForecasts'][3]['Date']
+
+	day5 = forcast_info['DailyForecasts'][4]['Date']
+
+
+	print(day1,day2,day3,day4,day5)
+	return render_template('search.html',title='Search',form=form)
